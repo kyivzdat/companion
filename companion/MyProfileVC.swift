cbae48e8a1f32047e3dc0b54a8bdb9539dd11c82
Vladyslav PALAMARCHUK <kyivzdat@gmail.com>
2019-11-01 19:58:19 +0200

    update searchBar to iOS11 standart

--- companion/MyProfileVC.swift
+++ companion/MyProfileVC.swift
@@ -10,79 +10,44 @@ import UIKit
 
 class MyProfileVC: UIViewController, UISearchBarDelegate {
 
-    @IBOutlet weak var searchButtonOutlet: UIBarButtonItem!
-    @IBOutlet var searchBar: UISearchBar!
 
     var resultSearchController: UISearchController?
-    
-//    let resultSearchController = UISearchController(searchResultsController: TableVC)
+    var searchBar: UISearchBar?
     
     let iconSearch = UIImage(named: "iconSearch")
-    var profileLabel : UILabel?
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        searchBar.delegate = self
-        initProfileLabel()
-        
-        
-        let table = storyboard?.instantiateViewController(withIdentifier: "TableVC") as! TableVC
-        resultSearchController = UISearchController(searchResultsController: table)
-        resultSearchController?.searchResultsUpdater = table
+        let tableView = storyboard?.instantiateViewController(withIdentifier: "TableVC") as! TableVC
+        resultSearchController = UISearchController(searchResultsController: tableView)
+        resultSearchController?.searchResultsUpdater = tableView
+
         searchBar = resultSearchController?.searchBar
-    
-//        resultSearchController?.hidesNavigationBarDuringPresentation = false
-//        resultSearchController?.dimsBackgroundDuringPresentation = true
-//        definesPresentationContext = true
+        searchBar?.delegate = self
+        searchBar?.sizeToFit()
+        searchBar?.placeholder = "Search a user"
+        
+        resultSearchController?.hidesNavigationBarDuringPresentation = true
+        resultSearchController?.dimsBackgroundDuringPresentation = true
+        definesPresentationContext = true
+        navigationItem.searchController = resultSearchController
     }
 
-    func initProfileLabel() {
-        profileLabel = UILabel()
-        guard let profileLabel = profileLabel else { return }
-        profileLabel.text = "Profile"
-        profileLabel.font = UIFont.boldSystemFont(ofSize: 17)
-    }
 
     @IBAction func tapSearchButton(_ sender: UIBarButtonItem) {
-        showSearchBar()
+
+        self.resultSearchController?.searchBar.becomeFirstResponder()
     }
 
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        hideSearchBar()
-    }
-
+    
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
         guard searchBar.text?.isEmpty == false else { print("SearchBar is empty"); return }
-        
         apiInfo.getProfile(user: searchBar.text!.lowercased())
     }
-    
-    func showSearchBar() {
-        
-        self.navigationItem.rightBarButtonItem?.image = nil
-        searchBar.sizeToFit()
-        searchBar.alpha = 0
-        navigationItem.titleView = resultSearchController?.searchBar
-        UIView.animate(withDuration: 0.5, animations: {
-            self.searchBar.alpha = 1
-        }, completion: { finished in
-            self.searchBar.becomeFirstResponder()
-        })
-    }
-    
-    func hideSearchBar() {
-    
-        self.navigationItem.rightBarButtonItem?.image = iconSearch
-        guard let profileLabel = profileLabel else { return }
-        profileLabel.alpha = 0
-        UIView.animate(withDuration: 0.3, animations: {
-            self.navigationItem.titleView = self.profileLabel
-            profileLabel.alpha = 1
-        })
-    }
+
     
     func alert(title: String, message: String) {
         
@@ -115,3 +80,4 @@ extension UIApplication {
         return base
     }
 }
+
